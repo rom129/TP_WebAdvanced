12c9c44ec7617e77faccf2a15d7c5c8a39f61714
Tutileni <simaopedropedro1992@hotmail.com>
2017-10-07 15:32:15 +0200

    Réponse aux question 01E et finalization de la premiere partie

--- TP1_PEDRO_NARBONI/src/main/java/ex01C/Log4jBasics.java
+++ TP1_PEDRO_NARBONI/src/main/java/ex01C/Log4jBasics.java
@@ -2,7 +2,7 @@ package ex01C;
 import org.apache.log4j.*;
  
 public class Log4jBasics { 
-	protected static Logger log = Logger.getLogger(Log4jBasics.class);
+	protected static Logger log=Logger.getLogger(Log4jBasics.class);
 
 	public static void main(String[] args) { 
 		log.debug("DEBUG: Cool !"); 		// n'aparait que lorsqu'il y a la config log4j.rootLogger=DEBUG, dans log4j.properties
--- TP1_PEDRO_NARBONI/src/main/java/ex01E/ABC.java
+++ TP1_PEDRO_NARBONI/src/main/java/ex01E/ABC.java
@@ -1,36 +1,41 @@
-package ex01E; 
+package ex01E;
 
-class A { 
-	int x; 
-	
-	void m() { 
-		System.out.println("Je suis dans la méthode m d'une instance de A"); 
-	} 
+class A {
+    int x;
+    
+    void m() {
+        System.out.println("Je suis dans la méthode m d'une instance de A");
+    }
 }
 
-class B extends A { 
-	int x; 
-	
-	void m() { 
-		System.out.println("Je suis dans la méthode m d'une instance de B"); 
-	} 
+class B extends A {
+    int x;
+    
+    void m() {
+        System.out.println("Je suis dans la méthode m d'une instance de B");
+    }
 }
 
-class C extends B { 
+class C extends B {
 
-	int x, a; 
-	
-	void m() { 
-		System.out.println("Je suis dans la méthode m d'une instance de C"); 
-	} 
-	
-	void test() { 
-		a = super.x; 
-		a = super.super.x; // Ligne à corriger
-		a = ((B)this).x; 
-		a = ((A)this).x; 
-		super.m(); 
-		super.super.m();  // Ligne à corriger
-		((B)this).m(); // (1) 
-	} 
+    int x, a;
+    
+    void m() {
+        System.out.println("Je suis dans la méthode m d'une instance de C");
+    }
+    
+    void test() {
+        a = super.x;
+        a = super.x; // Ligne corrigé
+        a = ((B)this).x;
+        a = ((A)this).x;
+        super.m();
+        super.m(); // Ligne corrigé
+        ((B)this).m(); // (1)
+    }
 }
+
+//Questions :
+//-> ((B)this).m(); // (1) -->Correspond à l'execution de la methode m() de l'instance de l'object C
+//-> Oui, on s'attandais à avoir ce resultat puisqu'on fait reference à l'object courrant avec "this"
+//-> Il s'agit d'un polymorphisme de type ad hoc, puisqu'on utilise la surcharge de la methode m()
